47cc2ed0b26a50b84cdf1bb866324b12cd633f03
Dr. Under-Qualified <99283516+Heisenberg-UP@users.noreply.github.com>
2024-01-20 21:23:26 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ source env/bin/activate # Activates virtual environment
 
 python python/fakenewsscraper.py & python python/gui.py # Runs necessary python files
 
-rm csv/NEWS.csv # Removes NEW.csv data when program is stopped
+rm files/NEWS.csv # Removes NEW.csv data when program is stopped
 
 deactivate # Deactivates vitual environment
