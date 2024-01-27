e19deb754bfa8204897105281f91023af9ec5723
Heisenberg Uncertainty Principle <99283516+Heisenberg-UP@users.noreply.github.com>
2024-01-26 21:42:23 -0600

    Theme change, and adjustable winows

--- files/gui.py
+++ files/gui.py
@@ -6,34 +6,48 @@ from bs4 import BeautifulSoup
 
 
 # Functions
+def set_theme_and_open_window(layout, layout_x, layout_y, theme='DarkBlack1'):
+
+    sg.set_options(element_padding=(0, 0))
+    sg.theme(theme)
+    window = sg.Window('', layout, resizable=True, size=(layout_x, layout_y), background_color='black', grab_anywhere=True)
+    return window
+
+
+def getdata(url):
+
+    """
+    Function uses requests library to get URL and return its text.
+    """
+
+    r = requests.get(url) # Gets url data
+
+    return r.text # Returns text from url data
+
 def Article_Format(event):  # Set-Up New window for Article to read
 
     """
     Function creates new window that summarizes article, and provides a value to the factualness of the article.
     """
 
-    def getdata(url):  # Gets URL as a string
-
-        """
-        Function uses requests library to get the URL and returns it as text.
-        """
-    
-        r = requests.get(url)
-        return r.text
-
-    htmldata = getdata(df['Link'][event])
-    soup = BeautifulSoup(htmldata, 'html.parser')
+    htmldata = getdata(df['Link'][event]) # Gets url text
+    soup = BeautifulSoup(htmldata, 'html.parser') # Sparse data
     data = ''
-    for data in soup.find_all('div', attrs={'class': 'short-on-time'}):
-        data.get_text().strip()
+    for data in soup.find_all('div', attrs={'class': 'short-on-time'}): # Strips words by spaces
+        data.get_text().strip() 
 
-    layout_new = [[sg.T(df['Statement'][event], size=(55, None), text_color='#FF7F00')],
-                  [sg.T('If your time is short: ', size=(55, None), text_color='#FF7F00')],
-                  [sg.T(data.get_text().strip(), size=(60, None))],
-                  [sg.T('Source: ' + df['Source'][event])],
-                  [sg.T('Detection: ' + df['Label'][event])]]
-    values, events = sg.Window('Article', layout_new, size=(400, 300)).read(close=True)
-    return values, events
+    # Window layout
+    layout_new = [[sg.T(df['Statement'][event], size=(55, None), text_color='#FF7F00', background_color='black')],
+                  [sg.T('If your time is short: ', size=(55, None), text_color='#FF7F00', background_color='black')],
+                  [sg.T(data.get_text().strip(), size=(60, None), background_color='black')],
+                  [sg.T('Source: ' + df['Source'][event], background_color='black')],
+                  [sg.T('Detection: ' + df['Label'][event], background_color='black')]
+    ]
+
+    new_window = set_theme_and_open_window(layout_new, 400, 400)
+    new_window.read(close=True)
+
+    return
 
 
 # Uploading File from Fake News Scraper file
@@ -43,55 +57,51 @@ df = pd.DataFrame(NEWS, columns=['Statement', 'Link', 'Date', 'Source', 'Label']
 pd.set_option('display.max_columns', None) # Displays all of the columns
 
 # Re-Labelling labels
-df['Label'] = df['Label'].replace(['pants-fire'], 'Extremely False')
-df['Label'] = df['Label'].replace(['false'], 'False')
-df['Label'] = df['Label'].replace(['barely-true'], 'Barely True')
-df['Label'] = df['Label'].replace(['true'], 'True')
+label_mapping = {'pants-fire': 'Extremely False', 'false': 'False', 'barely-true': 'Barely True', 'true': 'True'}
+df['Label'] = df['Label'].replace(label_mapping)
 
 
-# Create GUI for showing the .csv file data
-sg.theme('Topanga')  # Theme for GUI
+# Set the theme to black and white
+sg.theme('DarkBlack1')
 
 # Basic Layout
-layout = [[sg.Text("All news is based on PolitiFact's website. \nPolitiFact.com is an American nonprofit project "
-                   "operated by the Poynter Institute in St. Petersburg, Florida, with offices there and in "
-                   "Washington, D.C. \n\n\nHere is a brief summary of the 5 most recent stories:", size=(90, None))],
-          [sg.B(df['Statement'][0], key='0')], # Story 1
-          [sg.B(df['Statement'][1], key='1')], # Story 2
-          [sg.B(df['Statement'][2], key='2')], # Story 3
-          [sg.B(df['Statement'][3], key='3')], # Story 4
-          [sg.B(df['Statement'][4], key='4')]] # Story 5
+layout = [
+    [sg.Text('FAKE NEWS FINDER', font=('Helvetica', 36), justification='center', size=(20, 1), background_color='black')],
+    [sg.Text("Stories from Politico", 
+             size=(90, None), font=('Helvetica', 14), text_color='dark grey', background_color='black', justification='center')],
+    [sg.Text("\n\nTOP 5 STORIES:",
+              size=(90, None), font=('Helvetica', 18), text_color='white', background_color='black', justification='center')],
+    [sg.B(df['Statement'][0], key='0', pad=(20, 10), size=(50, 2))], # Story 1
+    [sg.B(df['Statement'][1], key='1', pad=(20, 10), size=(50, 2))], # Story 2
+    [sg.B(df['Statement'][2], key='2', pad=(20, 10), size=(50, 2))], # Story 3
+    [sg.B(df['Statement'][3], key='3', pad=(20, 10), size=(50, 2))], # Story 4
+    [sg.B(df['Statement'][4], key='4', pad=(20, 10), size=(50, 2))]  # Story 5
+]
 
-window = sg.Window('Fake News Finder', layout, size=(600, 250)) # Sets Window Size and Label
+window = set_theme_and_open_window(layout, 400, 700)
 
-
-# If statement for program to run
+# If statement for the program to run
 if __name__ == "__main__":
-
     try:
-
-        # The Event Loop to run program
+        # The Event Loop to run the program
         while True:
             event, values = window.read()
 
-            # Assigns a key to Article_Format() Function for proper article opening
-            if event == '0':
-                Article_Format(0)
-            elif event == '1':
-                Article_Format(1)
-            elif event == '2':
-                Article_Format(2)
-            elif event == '3':
-                Article_Format(3)
-            elif event == '4':
-                Article_Format(4)
-
-            if event == sg.WIN_CLOSED or event == 'Exit': # Closes program
+            if event in ['Exit', sg.WIN_CLOSED]:  # Closes program
                 break
 
-    except KeyboardInterrupt:
+            if event.isdigit() and 0 <= int(event) < 5:
+                Article_Format(int(event))
 
-        SystemExit
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        window.close()
+
+# Explicitly set the theme for the new window
+sg.set_options(element_padding=(0, 0))
+sg.theme('DarkBlack1')
 
 
 window.close()
